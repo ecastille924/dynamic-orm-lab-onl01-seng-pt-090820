e211f5d77bedfabedcb429ecf1aed7e3dd3fc3f8
Erick Castille <ecastille924@gmail.com>
2020-11-16 23:53:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,7 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
   
-  def values_For_insert
+  def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless seld(col_name).nil?
