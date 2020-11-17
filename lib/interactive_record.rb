2578e80e178ea0a5706b0799fa9b932992264e68
Erick Castille <ecastille924@gmail.com>
2020-11-17 00:11:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,4 +57,9 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
   end
+  
+  def self.find_by()
+    sql = "SELECT * FROM "
+    DB[:conn].execute(sql, )
+  end
 end
